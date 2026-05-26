20a1a3ddf61ddb73bc3bb4bea56a17b705f08cf1
Jawad W. Ahmed <jawad.w.ahmed@gmail.com>
2026-05-26 01:21:01 +0100

    bump

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A pascal compiler, named after the legend Niklaus Emil Wirth";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
